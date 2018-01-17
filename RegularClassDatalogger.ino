77c73ca7201a8bcf37dff9489a7e6d282e53df7c
MatthewL-UM <liumat@umich.edu>
2018-01-16 22:36:25 -0500

    Initial Setup
    Main setup before changes.

--- RegularClassDatalogger.ino
+++ RegularClassDatalogger.ino
@@ -4,6 +4,7 @@
 //  - Altitude Measurement in Meters
 //  - Gyroscope Measurment in Rad/Sec
 //  - Accelerometer Measurment in Meters/Sec
+
 #include <XBee.h>
 #include <stdlib.h>
 
@@ -17,7 +18,11 @@
 #include "Data.h"
 #include <Wire.h>
 
-Data *data;
+//Initialize our data object
+Data *data = 0;
+
+//Initialize our xbee
+const HardwareSerial *xbeeSerial = &Serial1;
 
 // Hertz Rate for Data Collection
 const int hertz = 10;
@@ -35,16 +40,17 @@ void setup() {
 }
 
 void loop() {
-  // Blink LED and Write Data to Serial regularly
+  // Loop at a certain frequency
   if (millis() - lastLoopTime > delayTime) {
     lastLoopTime = millis();
 
     data->update();
-    writeData();
+    printData();
   }
 }
 
-void writeData() {
+//prints information to serial monitor
+void printData() {
   // MILLIS,ALTITUDE,GYROX,GYROY,GYROZ,ACCELX,ACCELY,ACCELZ,
   Serial.print(millis());
   Serial.print(',');
@@ -61,5 +67,27 @@ void writeData() {
   Serial.print(data->getAccelY());
   Serial.print(',');
   Serial.println(data->getAccelZ());
+}
+
+//Creates A message
+void messageA()
+{
+  // A,MX2,MILLIS,ALT_BARO,ANALOG_PITOT,PRESS,TEMP,DROP_TIME,DROP_ALT
+
+  
+}
+
+//Creates B message
+void messageB()
+{
+  // B,MX2,MILLIS,GPS_SYSTEM,LAT,LON,GPS_SPEED,GPS_COURSE,GPS_ALT,GPS_HDOP
+
+}
+
+//Creates C message
+void messageC()
+{
+  // C,MX2,MILLIS,GYROX,GYROY,GYROZ,ACCELX,ACCELY,ACCELZ
+
 }
 
